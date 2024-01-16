7adfefb1f6e808c95a6e9bf8fdb2eefe5d11c9c6
991creator <126946426+991creator@users.noreply.github.com>
2024-01-16 21:10:32 +0300

    secondDeploy

--- server.go
+++ server.go
@@ -1,14 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	router.GET("/chats", getUsers)
 	router.GET("/users", getUsers)
+	router.GET("/messages", getMessages)
 	router.POST("/chats", setChats)
 	router.POST("/users", setUsers)
-	router.Run()
+	router.POST("/messages", sendMessages)
+	router.Run(":1234")
 }
 
 func getUsers(c *gin.Context) {
@@ -18,11 +24,21 @@ func getUsers(c *gin.Context) {
 func getChats(c *gin.Context) {
 	c.String(200, "get chats!")
 }
-
+func getMessages(c *gin.Context) {
+	c.String(200, "get ,messages!")
+}
 func setUsers(c *gin.Context) {
 	c.String(200, "set users!")
 }
 
 func setChats(c *gin.Context) {
 	c.String(200, "set chats!")
+}
+
+func sendMessages(c *gin.Context) {
+	id := c.Query("id")
+	senderId := c.Query("sender")
+	message := c.Query("message")
+
+	fmt.Printf("Message sent with id %s and senderId %s and message $s", id, senderId, message)
 }
